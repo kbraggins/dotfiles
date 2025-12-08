7c16912b2ab0154ae36500aa604ecc3c050293ae
Kyler Bragg <braggky@gmail.com>
2025-12-07 20:22:32 -0500

    Added keybinds for running and stopping CMake

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- CMake keybinds
+vim.api.nvim_set_keymap("n", "<leader>cr", ":CMakeRun<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>ce", ":CMakeStopRunner<CR>", { noremap = true, silent = true })
